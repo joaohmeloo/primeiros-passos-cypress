9ee318adb934046d58314df0f310a70aa8faa188
Joao Melo <joaohmelo22@gmail.com>
2024-07-30 08:04:10 -0300

    User Updated Scenario Added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -5,13 +5,28 @@ describe('Orange GRM Tests', () => {
 
 
 
-  it('Login - Success', () => {
+  it.only ('User Info Update - Success', () => {
     cy.visit('auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    
+
+    cy.get(selectorsList.firstNameField).clear().type("Teste de escrita")
+    cy.get(selectorsList.lastNameField).clear().type("teste last name")
+    cy.get(selectorsList.genericField).eq(3).clear().type("Emploree ID")
+    cy.get(selectorsList.genericField).eq(4).clear().type("Other ID")    
+    cy.get(selectorsList.genericField).eq(5).clear().type("Drivers License")
+    cy.get(selectorsList.genericField).eq(6).clear().type("2024/07/29")
+    cy.get(selectorsList.dateCloseButton).click()
+    
+    //cy.get(selectorsList.saveButton).eq(0).click()
+    cy.get('.oxd-button--medium').eq(0).click()
+    cy.get('body').should('contain', 'Success')
+
   })
 
   it('Login - Fail (Wrong password)', () => {
